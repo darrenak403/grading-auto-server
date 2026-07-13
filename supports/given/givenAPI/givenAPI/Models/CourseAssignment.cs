2a34d42a07eebf40f0d33b48848265ee85516473
darrenak403 <ngothanhdat4002@gmail.com>
2026-07-13 10:19:38 +0700

    chore: stop tracking supports/, add supports-su26/ for push

--- supports/given/givenAPI/givenAPI/Models/CourseAssignment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace givenAPI.Models
-{
-    public partial class CourseAssignment
-    {
-        public int CourseId { get; set; }
-        public int InstructorId { get; set; }
-        public DateTime? AssignmentDate { get; set; }
-    }
-}
